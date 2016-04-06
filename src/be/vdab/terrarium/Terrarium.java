2d69b64e76136a29284d8e0aee61b83212a48460
ArneSimons <arne.simons@skynet.be>
2016-04-06 11:11:03 +0200

    Commit 06/04/2015

---
+++ src/be/vdab/terrarium/Terrarium.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.vdab.terrarium;
+
+import be.vdab.terrarium.items.Carnivoor;
+import be.vdab.terrarium.items.Ding;
+import be.vdab.terrarium.items.Herbivoor;
+import be.vdab.terrarium.items.Plant;
+
+/**
+ *
+ * @author arne.simons
+ */
+public class Terrarium {
+    private final int aantalPlanten; //default 60
+    private final int aantalHerbivoren; //default 30
+    private final int aantalCarnivoren; //default 10    
+    private final int x; //horizontaal, default 20
+    private final int y; //verticaal, default 20
+    private Speelveld speelveld;
+
+    public Terrarium(int aantalPlanten, int aantalHerbivoren, int aantalCarnivoren, int x, int y) {
+        this.aantalPlanten = aantalPlanten;
+        this.aantalHerbivoren = aantalHerbivoren;
+        this.aantalCarnivoren = aantalCarnivoren;
+        this.x = x;
+        this.y = y;
+        
+        speelveld = new Speelveld(x,y);
+        
+        for(int i = 1; i <= aantalPlanten; i++) {
+            speelveld.dingToevoegen((Ding)new Plant());
+        }
+        
+        for(int i = 1; i <= aantalHerbivoren; i++) {
+            speelveld.dingToevoegen((Ding)new Herbivoor());
+        }
+        
+        for(int i = 1; i <= aantalCarnivoren; i++) {
+            speelveld.dingToevoegen((Ding)new Carnivoor());
+        }
+        
+    }   
+
+    public Speelveld getSpeelveld() {
+        return speelveld;
+    }
+    
+}
