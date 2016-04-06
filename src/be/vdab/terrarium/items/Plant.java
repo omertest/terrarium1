e9f8ba0fbd8834b2d3f350c1a9dc24ff001f0746
ArneSimons <arne.simons@skynet.be>
2016-04-06 09:50:38 +0200

    Commit 06/04/2015

---
+++ src/be/vdab/terrarium/items/Plant.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.vdab.terrarium.items;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author arne.simons
+ */
+public class Plant extends Ding {
+
+    public Plant() {
+        super.setLevenswaarde(0);
+    }
+
+    @Override
+    public void verhoogLevenswaarde(long levenswaarde){
+        if (Long.MAX_VALUE - super.getLevenswaarde() > levenswaarde) {
+            super.setLevenswaarde(Long.MAX_VALUE);
+        } else {
+            super.setLevenswaarde(super.getLevenswaarde() + levenswaarde);
+        }
+    }
+    
+}
