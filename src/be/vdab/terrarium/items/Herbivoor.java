7e6dca6c6cae5961307b1db82d3fe6a9d9cc0733
DamonHill <damonhill2000@hotmail.com>
2016-04-07 11:27:56 +0200

    Update Herbivoor.java

--- src/be/vdab/terrarium/items/Herbivoor.java
+++ src/be/vdab/terrarium/items/Herbivoor.java
@@ -19,9 +19,6 @@ public class Herbivoor extends LevendWezen {
         Random lwRandom = new Random();
         super.setLevenswaarde((long) (lwRandom.nextInt(7) + 4));
     }
-    public void zetStap() {
-        super.setLevenswaarde(super.getLevenswaarde() - 1);
-    }
 
     @Override
     public void interageerMetEenDing(Ding ding) {
@@ -59,48 +56,7 @@ public class Herbivoor extends LevendWezen {
     }
     
     private void wandel() {
-        // te bekijken in carnivoor!!
-    }
-    
-    public void zetStap() {
         super.setLevenswaarde(super.getLevenswaarde() - 1);
-    }
-
-    @Override
-    public void interageerMetEenDing(Ding ding) {
-        ding.interageerMetEenHerbivoor(this);
-    }
-
-    @Override
-    public void interageerMetEenCarnivoor(Carnivoor carnivoor) {
-        carnivoor.interageerMetEenHerbivoor(this);
-    }
-
-    @Override
-    public void interageerMetEenHerbivoor(Herbivoor herbivoor) {
-        this.boemboem(herbivoor);
-    }
-
-    @Override
-    public void interageerMetEenPlant(Plant plant) {
-        this.eet(plant);
-    }
-
-    @Override
-    public void interageerMetNiets(Niets niets) {
-        this.wandel();
-    }
-    
-    private void boemboem(Herbivoor herbivoor) {
-        terrarium.maakNieuweHerbivoor();
-    }
-    
-    private void eet(Plant plant) {
-        setLevenswaarde(this.getLevenswaarde() + plant.getLevenswaarde());
-        plant.setLevenswaarde(0);
-    }
-    
-    private void wandel() {
         // te bekijken in carnivoor!!
     }
 }
